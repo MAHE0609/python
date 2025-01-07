0fadcbf25752dd0364dc925b4262b9b4c1f933ce
MAHE0609 <maheshkumarreddy1206@gmail.com>
2025-01-07 12:26:47 +0530

    Add files via upload

---
+++ Day 1 AD.py
@@ -0,0 +1,486 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #addition +
+# #subtraction -
+# #multipication *
+# #division /
+# #modulus %
+# #power **
+
+# In[1]:
+
+
+a=4
+b=2
+a+b
+
+
+# In[2]:
+
+
+a-b
+
+
+# In[3]:
+
+
+a*b
+
+
+# In[4]:
+
+
+a/b
+
+
+# In[7]:
+
+
+43%4
+
+
+# In[6]:
+
+
+a**b
+
+
+# In[8]:
+
+
+a
+
+
+# In[9]:
+
+
+b
+
+
+#  #> < <= >= == !=
+
+# In[10]:
+
+
+a<b
+
+
+# In[11]:
+
+
+a>b
+
+
+# In[12]:
+
+
+a<10
+
+
+# In[13]:
+
+
+b>8
+
+
+# In[16]:
+
+
+a==4
+
+
+# In[17]:
+
+
+b==4
+
+
+# In[18]:
+
+
+a!=b
+
+
+# In[19]:
+
+
+b!=2
+
+
+# In[20]:
+
+
+a>=b
+
+
+# In[22]:
+
+
+a<=2
+
+
+# #logiacl operators
+# and
+# or
+# not
+
+# In[23]:
+
+
+a = 50
+b = 100
+
+
+# In[24]:
+
+
+a
+
+
+# In[25]:
+
+
+b
+
+
+# In[26]:
+
+
+a>b
+
+
+# In[27]:
+
+
+a<b
+
+
+# In[28]:
+
+
+a>100
+
+
+# In[31]:
+
+
+b<150
+
+
+# In[32]:
+
+
+(a>b) and (a<100)
+
+
+# In[33]:
+
+
+(a>b) or (a<100)
+
+
+# True and True --- True
+# True and False --- False
+# False and True --- False
+# False and False --- False
+
+# True or True --- True
+# True or False --- True
+# False or True --- True
+# False or False --- False
+
+# # q1
+# x = 34.56
+# y = 3.5
+# 
+# #all arithamatic operators
+# #all comparison operators
+# #and or 
+
+# In[46]:
+
+
+x=34.56
+y=3.5
+
+
+# In[48]:
+
+
+s=x+y
+s
+
+
+# In[1]:
+
+
+m=[203,'Hyderabad',45,True,'Mumbai',78,175,34.9]
+m
+
+
+# In[2]:
+
+
+m[-6]
+
+
+# In[3]:
+
+
+m[5]
+
+
+# In[4]:
+
+
+m[1]
+
+
+# In[5]:
+
+
+m[-1]
+
+
+# In[6]:
+
+
+m[2]
+
+
+# In[7]:
+
+
+m[-2]
+
+
+# In[8]:
+
+
+m[3]
+
+
+# In[9]:
+
+
+m[-3]
+
+
+# In[10]:
+
+
+m[4]
+
+
+# In[11]:
+
+
+m[1:6]
+
+
+# In[15]:
+
+
+m[-5]
+
+
+# In[16]:
+
+
+m[2:7]
+
+
+# In[17]:
+
+
+m[0:6]
+
+
+# In[18]:
+
+
+m[1:-2]
+
+
+# In[19]:
+
+
+m[2:-1]
+
+
+# In[20]:
+
+
+m[0:6:2]
+
+
+# In[7]:
+
+
+m=[203,'Hyderabad',45,True,'Mumbai',78,175,34.9]
+m
+
+
+# In[8]:
+
+
+m[-7:7]
+
+
+# In[25]:
+
+
+m[3:8:1]
+
+
+# In[9]:
+
+
+m
+
+
+# In[10]:
+
+
+m[-7:4]
+
+
+# In[11]:
+
+
+m[-2:2:-1]
+
+
+# In[12]:
+
+
+m.append(69)
+
+
+# In[13]:
+
+
+m
+
+
+# In[14]:
+
+
+m.append('INDIA')
+m
+
+
+# In[19]:
+
+
+m.insert(2,70)
+
+
+# In[21]:
+
+
+print(m)
+
+
+# In[23]:
+
+
+m.pop(7)
+
+
+# In[24]:
+
+
+m
+
+
+# In[25]:
+
+
+m.pop(6)
+
+
+# In[26]:
+
+
+m
+
+
+# In[32]:
+
+
+m.pop(4)
+
+
+# In[33]:
+
+
+m
+
+
+# In[42]:
+
+
+m=[203, 'Hyderabad', 70, 90, True, 78, 175, 34.9, 69, 'INDIA']
+m
+
+
+# In[43]:
+
+
+d=[4,5,6,7,]
+d
+
+
+# In[44]:
+
+
+a=[45,56,67]
+a
+
+
+# In[45]:
+
+
+d + a
+
+
+# In[47]:
+
+
+d.extend(a)
+d
+
+
+# In[48]:
+
+
+d.pop(6)
+
+
+# In[49]:
+
+
+d
+
+
+# In[51]:
+
+
+d.append(69)
+d
+
+
+# In[52]:
+
+
+d.pop(10)
+d
+
+
+# In[ ]:
+
+
+
+
