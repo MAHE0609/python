55454fc3d8e5356e577f6ed51d510ae9d622bdf3
MAHE0609 <maheshkumarreddy1206@gmail.com>
2025-01-09 11:40:55 +0530

    Add files via upload

---
+++ Day5 AD.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+if condition1:
+    task1 block of code
+elif condition2:
+    task2 block of code
+elif condition3:
+    task3 bloc of code
+else condition4:
+    task4 black of code
+
+
+# In[ ]:
+
+
+write a program to know number is positive,negative or zero
+
+
+# In[7]:
+
+
+x=6
+if x>0:
+    print('positive')
+elif x<0:
+    print('negative')
+else:
+    print('zero')
+
+
+# In[9]:
+
+
+y=10
+if y>5:
+    print('M')
+elif y>4:
+    print('s')
+elif y>7:
+    print('L')
+else:
+    print('O')
+
+
+# In[10]:
+
+
+s='Hyderabad'
+type(s)
+
+
+# In[12]:
+
+
+s[-3]
+
+
+# In[15]:
+
+
+s[-2:2:-1]
+
+
+# In[16]:
+
+
+s.upper()
+
+
+# In[17]:
+
+
+t='Hyderabad45'
+
+
+# In[18]:
+
+
+type(t)
+
+
+# In[28]:
+
+
+x='telangana'
+x='Tiger'
+x='Tin'
+
+
+# In[29]:
+
+
+type(x)
+
+
+# In[30]:
+
+
+x='lion'
+x='mumbai45'
+
+
+# In[32]:
+
+
+type(x)
+
+
+# In[33]:
+
+
+x='telangana'
+if x[0]=='t' or x[0]=='T' :
+    print('Yes')
+else:
+    print('No')
+
+
+# In[37]:
+
+
+x='tiger'
+if x[0].lower()=='t' :
+    print("Yes")
+else:
+    print('No')
+
+
+# In[42]:
+
+
+x[0].lower()=='t'
+
+
+# In[ ]:
+
+
+s,h,m
+
+
+# In[46]:
+
+
+if x[0].lower() in ('s','h','m'):
+    print('Yes')
+else:
+    print('No')
+
+
+# In[ ]:
+
+
+
+
